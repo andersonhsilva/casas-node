09b0ed39bc5b1626e8d68278c45c597932bdbf4c
Anderson <anderson.h.silva@gmail.com>
2026-06-01 12:16:10 -0300

    conexao com o mysql

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import connection from './database';
 
 const routes = new Router();
 
 routes.get('/', (req, res) => {
   return res.json({ ok: true });
+});
+
+routes.get('/casas', async (req, res) => {
+    const [rows] = await connection.query('SELECT * FROM casas');
+    return res.json(rows);
 });
 
 export default routes;
